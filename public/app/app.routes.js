a73dbe7639a2e30b087106693185f8affe5e96c3
Engineering Students <engineeringstudents@Engineering-Students-MacBook-Pro.local>
2018-03-16 12:53:42 -0400

    fixed login input field

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -4,6 +4,7 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function ($routeProvider){
 	$routeProvider.when('/login', {
 		templateUrl: 'app/views/login.html',
+		controller: 'loginController',
 		access: {
 			restricted: false,
 			student_access: false
--- public/app/controllers/loginController.js
+++ public/app/controllers/loginController.js
@@ -5,11 +5,18 @@ angular
 loginController.$inject = ['$scope', '$location'];
 
 function loginController($scope, $location){
+	$scope.login = login;
 	activate();
 
 	///////////
 
 	function activate(){
+	}
+
+	function login(email, password){
+		console.log('login attempt')
+		console.log(email)
+		console.log(password)
 	}
 
 }
