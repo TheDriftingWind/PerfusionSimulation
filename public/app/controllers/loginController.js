d36e03b7f87e534d8a1801dc396226fb0042115a
Engineering Students <engineeringstudents@engineedentsmbp.quinnipiac.edu>
2018-03-23 12:54:31 -0400

    handle failed login

--- public/app/controllers/loginController.js
+++ public/app/controllers/loginController.js
@@ -15,9 +15,14 @@ function LoginController($scope, $location, $route, AuthFactory){
 	}
 
 	function login(email, password){
+		$scope.loginForm.password = '';
+		$scope.loginForm.email = '';
 		AuthFactory.login(email, password).then(function(res){
-			if(res.data.user){
+			if(res.data.isValid){
 				$location.path('/waiting-room');
+	          	$route.reload();
+			}else{
+				$location.path('/login');
 	          	$route.reload();
 			}
 		}).catch(error => console.log('reject'));
